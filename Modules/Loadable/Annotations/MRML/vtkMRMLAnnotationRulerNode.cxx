b5a34d3c9b110956534b475abae26343e8f82652
Johan Andruejol <johan.andruejol@kitware.com>
2019-08-07 16:29:34 -0400

    [MuriPlan] Allow to enable/disable separate points in the line annotation

--- Modules/Loadable/Annotations/MRML/vtkMRMLAnnotationRulerNode.cxx
+++ Modules/Loadable/Annotations/MRML/vtkMRMLAnnotationRulerNode.cxx
@@ -27,6 +27,8 @@ vtkMRMLAnnotationRulerNode::vtkMRMLAnnotationRulerNode()
   this->SetDistanceAnnotationFormat("%.0f mm");
   this->ModelID1 = nullptr;
   this->ModelID2 = nullptr;
+  this->Position1Enabled = true;
+  this->Position2Enabled = true;
 }
 
 //----------------------------------------------------------------------------
@@ -74,6 +76,8 @@ void vtkMRMLAnnotationRulerNode::WriteXML(ostream& of, int nIndent)
     of << " modelID2=\"" << this->ModelID2 << "\"";
     }
   of << " distanceMeasurement=\"" << this->GetDistanceMeasurement() << "\"";
+  of << " position1Enabled=\"" << this->GetPosition1Enabled() << "\"";
+  of << " position2Enabled=\"" << this->GetPosition2Enabled() << "\"";
 }
 
 
@@ -108,6 +112,14 @@ void vtkMRMLAnnotationRulerNode::ReadXMLAttributes(const char** atts)
       {
       this->SetModelID2(attValue.c_str());
       }
+    else if (!strcmp(attName, "position1Enabled"))
+      {
+      this->SetPosition1Enabled(attValue.c_str());
+      }
+    else if (!strcmp(attName, "position2Enabled"))
+      {
+      this->SetPosition2Enabled(attValue.c_str());
+      }
     }
   this->EndModify(disabledModify);
 }
@@ -124,6 +136,8 @@ void vtkMRMLAnnotationRulerNode::Copy(vtkMRMLNode *anode)
   //this->SetDistanceAnnotationFormat(node->GetDistanceAnnotationFormat());
   //this->SetModelID1(node->GetModelID1());
   //this->SetModelID2(node->GetModelID2());
+  //this->SetPosition1Enabled(node->GetPosition1Enabled());
+  //this->SetPosition2Enabled(node->GetPosition2Enabled());
 }
 
 //-----------------------------------------------------------
@@ -177,6 +191,8 @@ void vtkMRMLAnnotationRulerNode::PrintAnnotationInfo(ostream& os, vtkIndent inde
   os << indent << "Model 1: " << (this->ModelID1 ? this->ModelID1 : "none") << "\n";
   os << indent << "Model 2: " << (this->ModelID2 ? this->ModelID2 : "none") << "\n";
 
+  os << indent << "Position 1 enabled: " << this->Position1Enabled << "\n";
+  os << indent << "Position 2 enabled: " << this->Position2Enabled<< "\n";
 }
 
 //---------------------------------------------------------------------------
