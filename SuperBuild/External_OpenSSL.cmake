xstrahl/Slicer

commit ddeb0a33ac36b7ea60a2ae4d16ecaafa45dfdf6c

COMP: Fix OpenSSL build on Mac 10.15 XCode 11.4.1 (#4857)
CMake computes the value of the -isysroot flag for cmake builds.
OpenSSL does not have a mechanism for setting this directly, so
using an environmental variable allows the compiler to pick
up the correct SDKROOT path.

Allow the SDKROOT to be found on mac when building OpenSSL.
This is needed to find standard header files and avoid errors like

```
 ./cryptlib.h:62:11: fatal error: 'stdlib.h' file not found
 # include <stdlib.h>
```

Related to #4681

SuperBuild/External_OpenSSL.cmake
@@ -108,6 +108,16 @@ ExternalProject_Execute(${proj} \"configure\" sh config --with-zlib-lib=${_zlib_
 ")
 
     #------------------------------------------------------------------------------
+    set(BUILD_COMMAND_MAKE_ENV)
+    if(APPLE)
+       # Allow the SDKROOT to be found on mac when
+       # building OpenSSL. This is needed to find
+       # standard header files and avoid errors like
+       # | ./cryptlib.h:62:11: fatal error: 'stdlib.h' file not found
+       # | # include <stdlib.h>
+       # |
+       set(BUILD_COMMAND_MAKE_ENV "SDKROOT=${CMAKE_OSX_SYSROOT}")
+    endif()
     ExternalProject_Add(${proj}
       ${${proj}_EP_ARGS}
       URL ${OpenSSL_${OPENSSL_DOWNLOAD_VERSION}_URL}
@@ -117,7 +127,7 @@ ExternalProject_Execute(${proj} \"configure\" sh config --with-zlib-lib=${_zlib_
       BUILD_IN_SOURCE 1
       PATCH_COMMAND ${CMAKE_COMMAND} -P ${_configure_script}
       CONFIGURE_COMMAND ""
-      BUILD_COMMAND make -j1 build_libs
+      BUILD_COMMAND ${BUILD_COMMAND_MAKE_ENV} make -j1 build_libs
       INSTALL_COMMAND ""
       DEPENDS
         ${${proj}_DEPENDENCIES}

commit bb3e4a6aa1b4fd043256fa19114f4511a0e74dc3

[Backport] COMP: Fix OpenSSL build on macOS setting SDKROOT in build script (PR-4883)
Cherry picked from commit Slicer/Slicer@1bc83586f

This commit fixes a regression introduced in ddeb0a33a (COMP: Fix
OpenSSL build on Mac 10.15 XCode 11.4.1 (PR-4857)) originally intended
to fix issue #4681

Setting the SDKROOT environment variable worked as expected on a regular
developer build but failed on a dashboard build where the invocation of
"make" is wrapped using a "launcher" [1]

This commit addresses the issue by writing a CMake script that will set
the environment and then invoke make.

Last, since  file descriptors expect by the make jobserver are not kept
open by the CMake process, the build script explicitly unset MAKEFLAGS
to avoid "warning: -jN forced in submake: disabling jobserver mode."
For more details, see [2]

[1] https://cmake.org/cmake/help/latest/manual/ctest.1.html#ctest-build-step
[2] https://stackoverflow.com/questions/29910944/is-it-possible-to-pass-through-gnu-make-jobserver-environment-to-a-submake-ser

SuperBuild/External_OpenSSL.cmake
@@ -91,6 +91,17 @@ set(ENV{VS_UNICODE_OUTPUT} \"\")
   # This is required when building Slicer using a cronjob where the
   # default environment is restricted.
   set(ENV{PATH} \"/usr/sbin:\$ENV{PATH}\")
+
+  # Allow the SDKROOT to be found on macOS when building OpenSSL.
+  # For more details, see https://github.com/Slicer/Slicer/issues/4681.
+  # This is needed to find standard header files and avoid errors
+  # like the following:
+  # | ./cryptlib.h:62:11: fatal error: 'stdlib.h' file not found
+  # | # include <stdlib.h>
+  # |
+  if(NOT DEFINED ENV{SDKROOT})
+    set(ENV{SDKROOT} \"\${CMAKE_OSX_SYSROOT}\")
+  endif()
 ")
     endif()
 
@@ -107,17 +118,17 @@ ExternalProject_Execute(${proj} \"configure\" sh config --with-zlib-lib=${_zlib_
   )
 ")
 
+    # build step
+    set(_build_script ${CMAKE_BINARY_DIR}/${proj}_build_step.cmake)
+    file(WRITE ${_build_script}
+"include(\"${_env_script}\")
+# Unset MAKEFLAGS to avoid \"warning: -jN forced in submake: disabling jobserver mode.\"
+unset(ENV{MAKEFLAGS})
+set(${proj}_WORKING_DIR \"${EP_SOURCE_DIR}\")
+ExternalProject_Execute(${proj} \"build\" make -j1 build_libs)
+")
+
     #------------------------------------------------------------------------------
-    set(BUILD_COMMAND_MAKE_ENV)
-    if(APPLE)
-       # Allow the SDKROOT to be found on mac when
-       # building OpenSSL. This is needed to find
-       # standard header files and avoid errors like
-       # | ./cryptlib.h:62:11: fatal error: 'stdlib.h' file not found
-       # | # include <stdlib.h>
-       # |
-       set(BUILD_COMMAND_MAKE_ENV "SDKROOT=${CMAKE_OSX_SYSROOT}")
-    endif()
     ExternalProject_Add(${proj}
       ${${proj}_EP_ARGS}
       URL ${OpenSSL_${OPENSSL_DOWNLOAD_VERSION}_URL}
@@ -127,7 +138,7 @@ ExternalProject_Execute(${proj} \"configure\" sh config --with-zlib-lib=${_zlib_
       BUILD_IN_SOURCE 1
       PATCH_COMMAND ${CMAKE_COMMAND} -P ${_configure_script}
       CONFIGURE_COMMAND ""
-      BUILD_COMMAND ${BUILD_COMMAND_MAKE_ENV} make -j1 build_libs
+      BUILD_COMMAND ${CMAKE_COMMAND} -P ${_build_script}
       INSTALL_COMMAND ""
       DEPENDS
         ${${proj}_DEPENDENCIES}