a2e720678b900b1d939547aa5e2df170ee854b3e
Sadhana Ravikumar <sadhana.ravikumar@kitware.com>
2025-09-18 15:09:58 -0400

    [Muriplan] Use Muriplan's fork of VTK This change was previously in Muriplan's top-level CMakeLists.txt

--- SuperBuild/External_VTK.cmake
+++ SuperBuild/External_VTK.cmake
@@ -139,7 +139,8 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
     set(_git_tag "454bb391dff78c6ff463298a5143ab5b4f0aa083") # slicer-v9.4.2-2025-03-26-13acb1a5d
     set(vtk_dist_info_version "9.4.2")
   elseif("${Slicer_VTK_VERSION_MAJOR}.${Slicer_VTK_VERSION_MINOR}" STREQUAL "9.5")
-    set(_git_tag "e21c90bd874fb15f1dc34986c238462b8aab4af8") # slicer-v9.5.2-2025-09-16-7c0494a68
+    set(Slicer_${proj}_GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/xstrahl/VTK.git")
+    set(_git_tag "39834381b31d237b013454a5598b59f623c5ee5d") # muriplan-slicer-v9.5.2-2025-09-16-7c0494a68
     set(vtk_dist_info_version "9.5.2")
   else()
     message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR.Slicer_VTK_VERSION_MINOR: ${Slicer_VTK_VERSION_MAJOR}.${Slicer_VTK_VERSION_MINOR}")
